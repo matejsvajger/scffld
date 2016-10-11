4d986dd168fef7d9f347679032a044e1c653730d
Matej Svajger <hello@matejsvajger.com>
2016-10-11 09:38:07 +0200

    Added browesrSync to build process.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -5,6 +5,7 @@ import gulp from 'gulp';
 import debug from 'gulp-debug';
 import pjson from './package.json';
 import minimist from 'minimist';
+import bs from 'browser-sync';
 
 //- Import all gulp-* based plugins
 import gulpLoadPlugins from 'gulp-load-plugins';
@@ -14,6 +15,7 @@ const plugins = gulpLoadPlugins();
 let args = minimist(process.argv.slice(2));
 let config = Object.assign({}, pjson.config);
 let target = args.dev ? config.directories.temporary : config.directories.build;
+let browserSync = bs.create();
 
 //- Load all gulp tasks
 let tasks = fs.readdirSync('./gulp');
@@ -22,7 +24,7 @@ for (let file of tasks) {
     plugins.util.log(
       'Requiring ' + plugins.util.colors.magenta(file.split('.').shift().toUpperCase()) + ' task module.'
     );
-    require(`./gulp/${file}`)(gulp, args, plugins, config, target);
+    require(`./gulp/${file}`)(gulp, args, plugins, config, target, browserSync);
   }
 }
 
@@ -32,9 +34,11 @@ gulp.task('default', ['clean'], () => {
 
 // Build production-ready code
 gulp.task('build', [
-  'less',
   'pug',
-  'browserify'
+  'less',
+  'browserify',
+  'browserSync',
+  'watch'
 ]);
 
 // serve, watch, share
