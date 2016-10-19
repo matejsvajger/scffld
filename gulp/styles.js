245c7e7eb8562716901fb22251ea162edecbfe6c
Matej Svajger <hello@matejsvajger.com>
2016-10-19 23:20:37 +0200

    Added node_modules and scffld/_modules dir to include paths for sass. Bugfix on source dir selection.

--- gulp/styles.js
+++ gulp/styles.js
@@ -7,17 +7,21 @@ export default function(gulp, args, $, config, bs) {
   let dirs = config.directories;
   let entries = config.entries;
   let pp = config.cssPreprocessor;
-  let src = typeof(dirs.styles) == 'string' ? dirs.styles.replace(/^_/, '') : dirs.styles.src;
+  let src = typeof(dirs.styles) == 'string' ? dirs.styles : dirs.styles.src;
   let dest = typeof(dirs.styles) == 'string' ? dirs.styles.replace(/^_/, '') : dirs.styles.dist;
       dest = path.join(dirs.build, dest);
 
   //- CSS Preprocessor setup
   let useSass = (pp == 'sass');
-  let fileExt = useSass ? '.(sass|scss)' : '.less';
+  let fileExt = useSass ? '.+(sass|scss)' : '.less';
   let preprocessor = useSass ? $.sass : $.less;
   let ppConfig = useSass ? {
       outputStyle: 'expanded',
-      precision: 10
+      precision: 10,
+      includePaths: [
+        path.join(dirs.source, '_modules'),
+        'node_modules'
+      ]
     } : { /* no config for less*/ };
 
   gulp.task('styles', () => {
@@ -25,9 +29,9 @@ export default function(gulp, args, $, config, bs) {
       browsers: ['last 2 version', '> 5%', 'safari 5', 'ios 6', 'android 4']
     };
 
-    return gulp.src( path.join(dirs.source, src, entries.css, fileExt) )
+    return gulp.src( path.join(dirs.source, src, entries.css + fileExt) )
       .pipe($.plumber())
-      .pipe($.debug({title:'LESS:'}))
+      .pipe($.debug({title:`${pp}:`}))
       .pipe($.sourcemaps.init())
       .pipe(preprocessor(ppConfig))
         .on('error', (err) => {
