7683d9d96190cadc2fb4743e6ae01ad4ea70990b
Matej Svajger <matej.svajger@koerbler.com>
2017-04-14 10:02:51 +0200

    Compile required ES6 scffld modules when fetched from node_modules.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,6 +2,7 @@
 
 import fs from 'fs';
 import gulp from 'gulp';
+import path from 'path';
 import pjson from './package.json';
 import minimist from 'minimist';
 import browserSync from 'browser-sync';
@@ -12,22 +13,28 @@ const plugins = LoadPlugins();
 const bs = browserSync.create();
 
 //- Load config into variable
-let config = Object.assign({}, pjson.config);
+let config = Object.assign({}, pjson.scffld);
 let argv = process.argv.slice(2);
 let args = minimist(argv);
     args.serve = (argv.shift() === 'serve');
 
 //- Load all gulp tasks
-let tasks = fs.readdirSync('./gulp');
+require('babel-register')({
+  only: 'node_modules/scffld'
+});
+
+let taskPath = './node_modules/scffld/gulp';
+  taskPath = (fs.existsSync(taskPath)) ? taskPath : './gulp';
+let tasks = fs.readdirSync(taskPath);
 for (let file of tasks) {
   if ((/\.(js)$/i).test(file)) {
     let task = file.split('.').shift();
     plugins.util.log(
       'Requiring task module ' + plugins.util.colors.magenta(task)
     );
-    require(`./gulp/${file}`)(gulp, args, plugins, config, bs);
+    var plugin = require('./' + path.join(taskPath, file));
+        plugin(gulp, args, plugins, config, bs);
   }
-}
 
 // Default task cleans build dir and rebuilds.
 gulp.task('default', ['clean'], () => {
